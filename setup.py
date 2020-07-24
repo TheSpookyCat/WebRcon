d40dbac9b0fbf7307955be48393ec58c9c842c51
Neko <nekoislewd@gmail.com>
2020-07-24 23:31:10 +0100

    Actually have requirements being a thing ;/
    Signed-off-by: Neko <nekoislewd@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
 with open('README.md', 'r') as fp:
     long = fp.read()
 
 setup(name='webrcon',
       author='lewdneko',
       url='https://github.com/lewdneko/webrcon',
-      version='1.0.0',
+      version='1.0.1',
       packages=['webrcon'],
       setup_requires=['wheel'],
       python_requires='>=3.6',
@@ -15,5 +18,7 @@ setup(name='webrcon',
       license='BlueOak-1.0.0',
       description='Basic async interface for Rust\'s WebRcon protocol',
       long_description=long,
-      keywords='rust webrcon rcon websockets ws rust_game async'
+      long_description_content_type='text/markdown',
+      keywords='rust webrcon rcon websockets ws rust_game async',
+      install_requires=install_requires
 )
