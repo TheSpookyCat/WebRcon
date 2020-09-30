5e2faa7a97f4e126bbf4d1f8b704be08f03a0af5
Neko <nekoislewd@gmail.com>
2020-09-30 13:09:06 +0100

    Bump version number
    Signed-off-by: Neko <nekoislewd@gmail.com>

--- webrcon/__init__.py
+++ webrcon/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'webrcon'
 __author__ = 'LewdNeko'
 __license__ = 'BSD-3'
 __copyright__ = 'Copyright 2020 LewdNeko'
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 
 from .connector import RconConnector
 from .exceptions import *
